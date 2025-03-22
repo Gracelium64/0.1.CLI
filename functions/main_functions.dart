925af0479983cd4e7191276396f861b7998cbef2
Grace <grace6424@icloud.com>
2025-03-22 18:39:13 +0100

    ...

--- functions/main_functions.dart
+++ functions/main_functions.dart
@@ -2,63 +2,6 @@ import '../version_number.dart';
 import 'functions_grace64.dart';
 import 'dart:io';
 
-void mainMenuCall() {
-  // clearConsole();
-  print(r'''
-  Main menu
-  ---------
-  (s)how lists
-  (a)dd task
-  (c)omlete task
-  (d)elete task
-  (e)xit''');
-}
-
-void listsMenuCall() {
-  print(r'''
-Lists menu
-----------
-(d)aily tasks
-(w)eekly tasks
-(D)eadline task
-(m)ain quest task
-(x) return to main menu
-''');
-}
-
-void addMenuCall(){
-  print(r'''
-What type of task do you want to add?
-(d)aily tasks
-(w)eekly tasks
-(D)eadline task
-(m)ain quest task
-(x) return to main menu
-''');
-}
-
-void completeMenuCall(){
-  print(r'''
-What type of task do you want to complete?
-(d)aily tasks
-(w)eekly tasks
-(D)eadline task
-(m)ain quest task
-(x) return to main menu
-''');
-}
-
-void deleteMenuCall() {
-  print(r'''
-What type of task do you want to delete?
-(d)aily tasks
-(w)eekly tasks
-(D)eadline task
-(m)ain quest task
-(x) return to main menu
-''');
-}
-
 void splashScreen() {
         clearConsole2();
         print('Initializing');
